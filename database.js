eeba821c4a291c2e47cb27ea56405c40baafe7d9
AndersonOls96 <11052@senacminas.edu.br>
2025-11-04 10:39:11 -0300

    finalização do banco de dados

--- database.js
+++ database.js
@@ -1,26 +1,27 @@
 const sqlite3 = require('sqlite3').verbose()
 const path = require('path')
 const { app } = require('electron')
+const { param } = require('express/lib/application')
 
-class Database{
-    constructor(){
+class Database {
+    constructor() {
         const userDataPath = app.getPath('userData')
         this.dbPath = path.join(userDataPath, 'contacts.db')
         this.db = null
     }
 
-    init(){
-        this.db = new sqlite3.Database(this.dbPath, (err)=>{
-            if(err){
+    init() {
+        this.db = new sqlite3.Database(this.dbPath, (err) => {
+            if (err) {
                 console.error('Erro ao abrir banco de dados', err)
-            }else{
+            } else {
                 console.log('Banco de dados conectado')
                 this.createTable()
             }
         })
     }
 
-    createTable(){
+    createTable() {
         const sql = `
             CREATE TABLE IF NOT EXISTS contacts (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -35,12 +36,114 @@ class Database{
             )
         `
 
-        this.db.run(sql, (err) =>{
-            if (err){
+        this.db.run(sql, (err) => {
+            if (err) {
                 console.error('Erro ao criar tabela', err)
-            }else{
+            } else {
                 console.log("Tabela criada/verificada com sucesso")
             }
         })
     }
+
+    getContacts(searchTerm = '') {
+        return new promises((resolve, reject) => {
+            let sql = 'SELECT * FROM contacts'
+            let params = []
+
+            if (searchTerm) {
+                sql += 'WHERE name LIKE ? OR email LIKE ? OR phone LIKE ? OR company LIKE ?'
+                const term = `%${searchTerm}%`
+                params = [term, term, term, term]
+            }
+
+            sql += 'ORDER BY name ASC'
+
+            this.db.all(sql, params, (err, rows) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(rows)
+                }
+            })
+        })
+    }
+
+    getContact(id) {
+        return new Promise((resolve, reject) => {
+            this.db.get('SELECT * FROM contacts WHERE id = ?', [id], (err, row) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(row)
+                }
+            })
+        })
+    }
+
+    addContact(contact) {
+        return new Promise((resolve, reject) => {
+            const sql = `
+                INSERT INTO contacts (name, email, phone, company, position, notes)
+                VALUES (?, ?, ?, ?, ?, ?)
+            `
+
+            const params = [
+                contact.name,
+                contact.email,
+                contact.phone,
+                contact.company,
+                contact.position,
+                contact.notes
+            ]
+
+            this.db.run(sql, params, function (err) {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve({ id: this.lastID })
+                }
+            })
+        })
+    }
+
+    updateContact(id, contact) {
+        return new Promise((resolve, reject) => {
+            const sql = `
+                UPDATE contacts
+                SET name = ?, email = ?, phone = ?, company = ?, position = ?, notes = ?, updated_at = ?,
+                WHERE id = ?
+            `
+
+            const params = [
+                contact.name,
+                contact.email,
+                contact.phone,
+                contact.company,
+                contact.position,
+                contact.notes
+            ]
+
+            this.db.run(sql, params, function (err) {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve({ changes: this.changes })
+                }
+            })
+        })
+    }
+
+    deleteContact(id) {
+        return new Promise((resolve, reject) =>{
+            this.db.run('DELETE FROM contacts WHERE id = ?', [id], function(err){
+                if(err){
+                    reject(err)
+                }else{
+                    resolve({ changes: this.changes })
+                }
+            })
+        })
+    }
 }
+
+module.exports = Database
